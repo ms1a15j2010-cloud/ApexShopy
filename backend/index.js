849519cff7dc0a947e755fc51571609167c8188e
Your Full Name <alikhan@gmail.com>
2026-08-26 14:16:10 +0500

    Fix static Vercel deployment

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/images", express.static(path.join(__dirname, "..", "images")));
+app.use("/images", express.static(path.join(__dirname, "..", "public", "images")));
 
 let mongoConnectionPromise = null;
 
